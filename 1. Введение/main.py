82748004334c315ff5b52da4f3e6356c51d2b620
Андрей Боев <a.boev.it@gmail.com>
2024-10-25 18:19:30 +0300

    1. Введение (примеры)

---
+++ 1. Введение/main.py
@@ -0,0 +1,21 @@
+"""
+print('Text 1', 'Text 2', 'Text 3', end=' ')
+print('Text 4', 'Text 5', 'Text 6')
+"""
+
+"""
+print('\'Text 1\'', '\'Text 2\'', '\'Text 3\'', '\'Text 4\'', sep='\n')
+"""
+
+"""
+name = input('Введите ваше имя: ')
+age = input('Введите ваш возраст: ')
+print(f'Привет, меня зовут {name}, мне {age} лет.')
+"""
+
+"""
+num1 = int(input('Введите первое число: '))
+num2 = int(input('Введите второе число: '))
+print(f'{num1} + {num2} = {num1 + num2}')
+"""
+
