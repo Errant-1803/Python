741f3b061fe378f19ffe8b62a521777807e824b6
Андрей Боев <a.boev.it@gmail.com>
2024-11-12 19:22:09 +0300

    Цикл For

---
+++ 4. Циклы/4.1 Цикл for/main.py
@@ -0,0 +1,14 @@
+"""
+print(list(range(10)))
+print(list(range(15, 50)))
+print(list(range(5, 100, 5)))
+"""
+
+"""   
+for i in range(10):
+    print(i, end=" ")
+"""
+total_sum = 0
+for num in range(10 + 1):
+    total_sum += num
+print(total_sum)
